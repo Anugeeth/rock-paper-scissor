5821fa36bf844c8a195b6e2f6d036636332f38ca
Anugeeth <anugeeth2@gmail.com>
2020-05-30 13:03:50 +0530

    added frontend code as submodule

--- index.js
+++ index.js
@@ -4,27 +4,27 @@ const io = require('socket.io')(server);
 
 server.listen(3000);
 
+// 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-
-let users_data = {};
+const usernames = {};
 
 io.on('connection', (socket) => {
     // join or create a new room
 
     socket.on('create or join', function (room_name, user_name) {
 
-        var clients = io.sockets.adapter.rooms[room_name];
+        let clients = io.sockets.adapter.rooms[room_name];
         if (clients) clients = clients.length;
-
         else clients = 0;
 
         if (clients <= 2) {
             socket.join(room_name);
             socket.emit('created', room_name, socket.id);
-            users_data[user_name] = socket.id;
+            socket.username = user_name;
+            usernames[username] = socket.id;
             console.log('created');
 
         }
@@ -35,7 +35,21 @@ io.on('connection', (socket) => {
     });
 
 
-    //
+    //when a move is triggered by user
+
+    socket.on('move', function (room_name, option) {
+        const user = users_data.find(element => element == socket.id);
+
+        socket.broadcast.to(room_name).emit()
+        
+    });
+
+    // on user disconnection
+
+    socket.on('disconnect', function () {
+        delete usernames[socket.username];
+        socket.broadcast.emit('userdisconnect', socket.username + 'left the game');
+    })
 })
 
 
